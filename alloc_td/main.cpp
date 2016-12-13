eb44aa1d8700f2bbcaf1f554a31c5c012b3f7ce3
Romain Amuat <romain.amuat@gmail.com>
2016-12-13 09:29:25 +0100

    Thorough tests

--- alloc_td/main.cpp
+++ alloc_td/main.cpp
@@ -10,9 +10,9 @@ int main () {
 
 	mvector m;
 	int * a = (int *) m.my_malloc(sizeof(int));
-	melement * h = (melement *) a - sizeof(melement);
+	melement * h = (melement *) (a - sizeof(melement));
 
-	std::cout << "Prev : [" << h->prev << "] - [" << h->adr-sizeof(melement) << "] - Next : [" << h->next << "]" << std::endl;
+	std::cout << "Prev : [" << h->prev << "] - [" << h->adr << "] - Next : [" << h->next << "]" << std::endl;
 	std::cout << "Taille : " << h->bloc_s << "\n\n" << std::endl;
 	
 	m.show();
