2349684ea688457349a2549a410d7289590df22f
Romain Amuat <romain.amuat@gmail.com>
2016-12-13 10:58:10 +0100

    Test malloc fait, on teste la reallocation

--- alloc_td/main.cpp
+++ alloc_td/main.cpp
@@ -11,16 +11,13 @@ int main () {
 	mvector m;
 	int * a = (int *) m.my_malloc(sizeof(int));
 	int * b = (int *) m.my_malloc(sizeof(int));
-	*a = 80;
-	std::cout << "a [" << a << "] : " << *a << std::endl;
-
-	melement * h = (((melement *) a)-1);
-
-	std::cout << "Prev : [" << h->prev << "] - [" << h->adr << "]/[" << h << "] - Next : [" << h->next << "]" << std::endl;
-	std::cout << "Taille : " << h->bloc_s << "\n\n" << std::endl;
 	
 	m.my_free(a);
 	m.my_free(b);
+
+	m.show();
+
+	int * c = (int *) m.my_malloc(sizeof(int));
 
 	m.show();
 
