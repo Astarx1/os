08f2cee05aadf6cf2c06b155dadbfa65bae97237
Romain Amuat <romain.amuat@gmail.com>
2016-12-13 09:24:31 +0100

    Test malloc

--- alloc_td/main.cpp
+++ alloc_td/main.cpp
@@ -6,9 +6,16 @@ int main () {
 	void * (* f_malloc)(size_t) = &malloc;
 	void (* f_free)(void *) = &free;
 
+	std::cout << "Test my_malloc" << std::endl;
+
 	mvector m;
-	m.malloc(10);
-	m.show();
+	int * a = (int *) m.my_malloc(sizeof(int));
+	melement * h = (melement *) a - sizeof(melement);
+
+	std::cout << "Prev : [" << h->prev << "] - [" << h->adr-sizeof(melement) << "] - Next : [" << h->next << "]" << std::endl;
+	std::cout << "Taille : " << h->bloc_s << "\n\n" << std::endl;
 	
+	m.show();
+
 	return 0;
 }
